9d54f1a9bd8ba2ac3b39532e9865d85dbbf30927
Leta-kasahun <letakasahun2@gmail.com>
2025-11-12 04:16:51 -0800

    day one

--- PHP-Projects/01_Basics/hello.php
+++ PHP-Projects/01_Basics/hello.php
@@ -1,3 +1,10 @@
 <?php 
-echo "Hellow world welcome to php course!";
+echo "Hellow world welcome to php course!\n";
+echo "this is php basics\n";
+ECHO "Hello PHP world\n";
+
+// this  is commented
+echo "commented";
+
 ?>
+
